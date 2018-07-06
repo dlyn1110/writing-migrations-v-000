5bb75cf95ee1a8a57f5b0fb86d788215690503ab
Dani Lyn <dlyn1110@gmail.com>
2018-07-06 00:39:56 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :artists, :favorite_food, :string
+    add_column :students, :favorite_food, :string
   end
 end
-
